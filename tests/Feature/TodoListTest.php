db82860f8d8a5ddbd0abe662607fa4b38f1ef01a
DipaWidia <wiartha2001@gmail.com>
2022-06-20 12:12:16 +0800

    create fetch todo-list api unit test

---
+++ tests/Feature/TodoListTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TodoListTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_store_todolist()
+    {
+        //preparation
+
+        //action
+        $response = $this->getJson(route('todo-list.store'));
+
+        //assertion
+        $this->assertEquals(1, $this->count($response->json()));
+    }
+}
