ca66c29925146e3a51c12ae67f2c54d7ed6ddfc3
DipaWidia <wiartha2001@gmail.com>
2022-06-23 08:02:29 +0800

    add unit test for login

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\Auth;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,14 +14,35 @@ class LoginTest extends TestCase
      *
      * @return void
      */
+    use RefreshDatabase;
 
     public function test_user_can_login()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson(route('user.login'),[
+            'email' => $user->email,
+            'password'=> 'password'
+        ])->assertOk();
+
+        $this->assertArrayHasKey('token', $response->json());
+    }
+
+    public function test_return_error_if_user_email_not_available()
     {
         $response = $this->postJson(route('user.login'),[
             'email' => 'user@gmail.com',
             'password'=> 'user123'
-        ])->assertOk();
+        ])->assertUnauthorized();
+    }
 
-        $this->assertArrayHasKey('token', $response->json());
+    public function test_raise_error_if_password_is_incorrect()
+    {
+        $user = User::factory()->create();
+
+        $response = $this->postJson(route('user.login'),[
+            'email' => $user->email,
+            'password'=> 'random'
+        ])->assertUnauthorized();
     }
 }
