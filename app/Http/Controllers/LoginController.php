e2432d8206f9b5d924ee4147f4994a6ee3e2116d
DipaWidia <wiartha2001@gmail.com>
2022-06-23 08:16:19 +0800

    add user generate token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -19,8 +19,10 @@ class LoginController extends Controller
                 return response('Credential not match', Response::HTTP_UNAUTHORIZED);
             }
 
+            $token = $user->createToken('api');
+
             return response([
-               'token' => 'hello'
+               'token' => $token->plainTextToken
             ]);
     }
 }
